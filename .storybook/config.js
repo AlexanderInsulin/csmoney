1dfbf42b1e567e85e362fe6da9e6d99e5b4e4f86
Alex <alex-top@1win.xyz>
2019-06-18 00:45:13 +0300

    - Make stories in storybook centered

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,9 @@
 import { configure, addParameters, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered/react';
+
+import '../src/index.css';
+
 addParameters({
   backgrounds: [
     { name: 'main', value: 'rgb(229, 229, 230)', default: true },
@@ -8,6 +12,7 @@ addParameters({
 })
 
 addDecorator(withKnobs)
+addDecorator(centered)
 
 const req = require.context('../src/components', true, /.*\.storie\.js$/);
 function loadStories() {
