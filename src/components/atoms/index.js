4e28caec59e5aafd63fb9762150c40ff2ce3c461
Alex <alex-top@1win.xyz>
2019-06-17 19:58:24 +0300

    - Wrap atoms in one module

---
+++ src/components/atoms/index.js
@@ -0,0 +1,4 @@
+export { default as Action } from './Action';
+export { default as Card } from './Card';
+export { default as Column } from './Column';
+export { default as Dropdown } from './Dropdown';
