e86dcec2516ecd69484158aed2e879600f7d3775
Alex <alex-top@1win.xyz>
2019-06-17 18:26:45 +0300

    - Fix knobs in sotrybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,13 @@
-import { configure, addParameters } from '@storybook/react';
- 
+import { configure, addParameters, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
 addParameters({
   backgrounds: [
     { name: 'main', value: 'rgb(229, 229, 230)', default: true },
     { name: 'white', value: '#ffffff' },
   ],
 })
+
+addDecorator(withKnobs)
 
 const req = require.context('../src/components', true, /.*\.storie\.js$/);
 function loadStories() {
