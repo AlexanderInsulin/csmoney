c018516b22acd3313c49be57fe82d51d6609bfcd
Alex <alex-top@1win.xyz>
2019-06-17 21:58:39 +0300

    - Add task molecule

---
+++ src/components/molecules/Task/Task.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Card, Action, Dropdown } from '../../atoms'
+
+const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+
+const Title = styled.h4`
+    all: unset;
+    font-weight: 900;
+    font-size: 16px;
+    margin: 0 0 10px 0;
+`
+
+const Description = styled.div`
+    all: unset;
+    font-size: 14px;
+    margin: 0 0 10px 0;
+`
+
+const Actions = styled.div`
+    display: flex;
+
+    & > *:not(:last-child) {
+        margin-right: 5px;
+    }
+`
+
+export default ({ title, description, otherBoards, onDelete, onMove }) => (
+    <Card>
+        <Layout>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+            <Actions>
+                <Action type={'danger'} onClick={onDelete}>Удалить</Action>
+                <Dropdown items={otherBoards} onChoose={(item => onMove(item.id))}>
+                    <Action type={'secondary'}>Переместить ↓</Action>
+                </Dropdown>
+            </Actions>
+        </Layout>
+    </Card>
+)
---
+++ src/components/molecules/Task/Task.storie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { text, boolean, array } from '@storybook/addon-knobs';
+
+import Task from './Task';
+
+
+
+storiesOf('molecules/Task', module)
+  .add('with text', () => (
+    <div style={{ width: '280px' }}>
+        <Task
+            title={text('title', 'Сделать всю работу')}
+            description={text('description', 'Нужно пойти на работу и попробовать закончить начатое')}
+            otherBoards={array('otherBoards', [ {id: 1, text: 'qwer'}, {id: 2, text: 'zxc'} ])}
+            onDelete={action('onDelete')}
+            onMove={action('onMove')}
+        />
+    </div>
+  ))
