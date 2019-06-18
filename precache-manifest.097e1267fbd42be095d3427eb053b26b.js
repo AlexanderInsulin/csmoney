09bef0073908fb1bf679186fa928c63b3373b133
Alex <alex-top@1win.xyz>
2019-06-18 14:26:24 +0300

    Updates

---
+++ precache-manifest.097e1267fbd42be095d3427eb053b26b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d728cb5cec0571bf6d1b1034939286c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8170dedf1a56d7019dd4",
+    "url": "/static/css/main.3e4208ac.chunk.css"
+  },
+  {
+    "revision": "a9a0ba58d67dae71c025",
+    "url": "/static/js/2.4e0670fa.chunk.js"
+  },
+  {
+    "revision": "8170dedf1a56d7019dd4",
+    "url": "/static/js/main.5cb0162d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
