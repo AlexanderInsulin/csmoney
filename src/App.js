304bc6ccce849f90cf9802cac0e98db1aa51f2e4
Alex <alex-top@1win.xyz>
2019-06-18 17:10:14 +0300

    - Change import style for actions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import BoardsList from './components/BoardsList'
-import { actions } from './store'
+import { deleteTask, moveTask, addTask } from './store/actions/taskActions'
 
 export default () => {
     const boards = useSelector(state => state)
     const dispatch = useDispatch()
 
     const onTaskDelete = ({ taskId, boardId }) =>
-        dispatch(actions.taskActions.deleteTask(taskId, boardId))
+        dispatch(deleteTask(taskId, boardId))
 
     const onTaskMove = ({ taskId, boardId, fromBoardId }) =>
-        dispatch(actions.taskActions.moveTask(taskId, boardId, fromBoardId))
+        dispatch(moveTask(taskId, boardId, fromBoardId))
 
     const onTaskAdd = ({ taskName, taskDescription, boardId }) =>
-        dispatch(actions.taskActions.addTask(taskName, taskDescription, boardId))
+        dispatch(addTask(taskName, taskDescription, boardId))
 
     return <BoardsList {...{ boards, onTaskDelete, onTaskMove, onTaskAdd }} />
 };
